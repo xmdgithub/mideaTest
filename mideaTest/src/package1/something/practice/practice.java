b2af781b2a48f21708f5acf9692a45ec3da09a55
Chmdxmd <chmdxmd@gmail.com>
2021-10-27 13:45:22 +0800

    hello*3

---
+++ mideaTest/src/package1/something/practice/practice.java
@@ -0,0 +1,9 @@
+package package1.something.practice;
+
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
